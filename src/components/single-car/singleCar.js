fc3f65c86b370007c1778b9a5520a27af0640a3d
cyberpolin <cyberpolin@gmail.com>
2017-01-17 15:46:02 -0600

    to share

---
+++ src/components/single-car/singleCar.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class SingleCar extends Component{
+  render(){
+    return(
+      <div className=>
+        <div>
+          <h2>Car name</h2><img src="/images/proveedor.png" alt="Nombre del proveedor"/>
+        </div>
+        <div>
+          <div>Datos generales</div>
+          <div>imagen del carro</div>
+        </div>
+        <div>Button</div>
+      </div>
+    )
+  }
+}
